cdb7d185cb8fec7887b5bbb04853b4cc6a152071
Evandro Oliveira <eoliveira@kenzan.com>
2016-08-21 20:08:21 -0300

    Heroku Procfile

--- src/main/java/br/com/eso/password/checker/app/config/JerseyConfig.java
+++ src/main/java/br/com/eso/password/checker/app/config/JerseyConfig.java
@@ -3,11 +3,13 @@ package br.com.eso.password.checker.app.config;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.springframework.stereotype.Component;
 
+import br.com.eso.password.checker.app.rest.PasswordCheckerRS;
+
 @Component
 public class JerseyConfig extends ResourceConfig {
 	
 	public JerseyConfig() {
-		packages("br.com.eso.password.checker.app.rest");
+		register(PasswordCheckerRS.class);
 	}
 
 }
