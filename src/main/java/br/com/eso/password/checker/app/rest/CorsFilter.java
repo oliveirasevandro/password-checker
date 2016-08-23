1444ee41527645fb4532b2a1fa869c71ec112099
Evandro Oliveira <eoliveira@kenzan.com>
2016-08-22 21:50:45 -0300

    CORS Filter

--- src/main/java/br/com/eso/password/checker/app/rest/CorsFilter.java
+++ src/main/java/br/com/eso/password/checker/app/rest/CorsFilter.java
@@ -31,7 +31,7 @@ public class CorsFilter implements Filter {
 		HttpServletResponse response = (HttpServletResponse) res;
 	    response.setHeader("Access-Control-Allow-Origin", "*");
 	    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-	    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+	    response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 	    response.setHeader("Access-Control-Max-Age", "3600");
 	    if (request.getMethod()!="OPTIONS") {
 	      chain.doFilter(req, res);
