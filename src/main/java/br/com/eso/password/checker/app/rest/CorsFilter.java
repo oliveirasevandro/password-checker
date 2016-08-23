607ca04611ca06b0f2e3da353e9d429760c9dcb0
Evandro Oliveira <eoliveira@kenzan.com>
2016-08-22 21:47:21 -0300

    CORS Filter

--- src/main/java/br/com/eso/password/checker/app/rest/CorsFilter.java
+++ src/main/java/br/com/eso/password/checker/app/rest/CorsFilter.java
@@ -27,16 +27,16 @@ public class CorsFilter implements Filter {
 	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		
-//		HttpServletRequest request = (HttpServletRequest) req;
-//		HttpServletResponse response = (HttpServletResponse) res;
-//			    response.setHeader("Access-Control-Allow-Origin", "*");
-//			    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
-//			    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
-//			    response.setHeader("Access-Control-Max-Age", "3600");
-//			    if (request.getMethod()!="OPTIONS") {
-//			      chain.doFilter(req, res);
-//			    } else {
-//			    }
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+	    response.setHeader("Access-Control-Allow-Origin", "*");
+	    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+	    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+	    response.setHeader("Access-Control-Max-Age", "3600");
+	    if (request.getMethod()!="OPTIONS") {
+	      chain.doFilter(req, res);
+	    } else {
+	    }
 		
 		
 	}
