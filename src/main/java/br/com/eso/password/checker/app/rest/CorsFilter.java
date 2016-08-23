66e1e2666685134eaf4f91bc4fad0892fa97ae8c
Evandro Oliveira <eoliveira@kenzan.com>
2016-08-22 21:28:23 -0300

    CORS Filter

---
+++ src/main/java/br/com/eso/password/checker/app/rest/CorsFilter.java
@@ -0,0 +1,51 @@
+package br.com.eso.password.checker.app.rest;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@Order(Ordered.HIGHEST_PRECEDENCE)
+class CorsFilter implements Filter {
+
+	@Override
+	public void destroy() {
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain)
+			throws IOException, ServletException {
+		
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+			    response.setHeader("Access-Control-Allow-Origin", "*");
+			    response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+			    response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
+			    response.setHeader("Access-Control-Max-Age", "3600");
+			    if (request.getMethod()!="OPTIONS") {
+			      chain.doFilter(req, res);
+			    } else {
+			    }
+		
+		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+	}
+
+
+
+
+}
